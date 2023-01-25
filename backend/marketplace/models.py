51214013d08193fb40b92dee0ab9b46ab3af9bf5
Soleil <soleilherring@gmail.com>
2023-01-25 15:06:43 -0800

    redid models

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=255)
-    username = models.CharField("Username", max_length=200)
+    username = models.CharField("Username", max_length=200, default='Unknown')
     email = models.EmailField()
     image = models.ImageField()
-    registrationDate = models.DateField("Registration Date", auto_now_add=True)
+    # registrationDate = models.DateField("Registration Date", auto_now_add=True)
 
     def __str__(self):
         return self.user
@@ -21,25 +21,24 @@ class Category(models.Model):
 
 class Item(models.Model):
     # CATEGORY = (('Painting'), ('Ceramics'), ('Knitting and Crocheting'), ('Illustration and Drawing'), ('Embroidery'), ('Sculpture'))
-    itemId= models.AutoField(primary_key=True)
     name = models.CharField("Name", max_length=50)
     description = models.TextField("Description", null=True,blank=True)
     image = models.ImageField()
+    condition = models.CharField(max_length=50, default='Unknown') 
+    category = models.CharField(max_length=50, default='Uncategorized')    
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    itemId = models.ForeignKey(Item, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
     reserved = models.BooleanField(default=False) 
 
     class Meta:
         ordering = ['reserved']
-
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    location = models.CharField(max_length=255)
-    date = models.DateTimeField(auto_now_add=True)
-
-
-
     
 
 class Review(models.Model):
