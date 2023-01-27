4ae78e816f712fc0cb08ce2f2cf205bee93e930b
Soleil <soleilherring@gmail.com>
2023-01-26 23:34:19 -0800

    imported redirect on views.py and imported os on settings

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import *
