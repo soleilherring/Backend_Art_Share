7d32916e35105d7c1f4938e8ad5828040b8bfe93
Soleil <soleilherring@gmail.com>
2023-01-26 20:43:39 -0800

    copied out media_url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,7 +138,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # # Base url to serve media files
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # # Path where media is stored
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
