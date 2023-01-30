8db257d38fc1aa10475306132843d44ec3a92325
Soleil <soleilherring@gmail.com>
2023-01-29 16:52:50 -0800

    added procfile, runtime txt for deployment, modifield admin and deleted comments from models

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -1,18 +1,11 @@
 from django.db import models
-# from cloudinary.models import CloudinaryField
-# from cloudinary.uploader import upload
 
-# uploaded_image = upload(file_path)
-# item = Item.objects.create(name=name, description=description, image=uploaded_image, condition=condition, owner=owner)
-# from django.contrib.auth.models import User
 
 class User(models.Model):
     name = models.CharField("Name", max_length=240)
     email = models.EmailField()
     location = models.CharField(max_length=255)
     password = models.CharField(max_length=200, default='Unknown')
-    # image = models.ImageField()
-    # registrationDate = models.DateField("Registration Date", auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -30,9 +23,6 @@ class Category(models.Model):
         ('Textiles', 'Textiles'),
     )
     name = models.CharField(max_length=200, choices=CATEGORY_CHOICES)
-    # name = models.CharField(max_length=200)
-    # description = models.TextField()
-    # item  = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -51,13 +41,8 @@ class Post(models.Model):
 class Item(models.Model):
     name = models.CharField("Name", max_length=50)
     description = models.TextField("Description", null=True,blank=True)
-    # image = models.FileField(upload_to='media/')
-    # image = models.ImageField('image', null=True, blank=True)
-    # image = models.ImageField()
-    # image = CloudinaryField('image')
     image = models.ImageField(upload_to='images/')
     condition = models.CharField(max_length=50, default='Unknown') 
-    # category = models.CharField(max_length=50, default='Uncategorized')    
     category = models.ManyToManyField(Category, related_name="items")
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     postId = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="items",blank=True, null=True)
